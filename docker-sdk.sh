86cd759cf0751f072e2a2213c8c7b9c95ffee12d
Paul Spooren <mail@aparcar.org>
2019-07-10 14:01:49 +0200

    Tag docker images with latest if x86-64/master
    This way one can run openwrtorg/rootfs instead of
    openwrtorg/rootfs:x86-64. This makes sense as it's the most common use
    case.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- docker-sdk.sh
+++ docker-sdk.sh
@@ -42,6 +42,10 @@ EOF
         if [ "$BRANCH" == "master" ]; then
             docker tag $DOCKER_IMAGE:$TARGET-$BRANCH $DOCKER_IMAGE:$TARGET
             docker push $DOCKER_IMAGE:$TARGET
+            if [ "$TARGET" == "x86-64" ]; then
+                docker tag $DOCKER_IMAGE:$TARGET-$BRANCH $DOCKER_IMAGE:latest
+                docker push $DOCKER_IMAGE:latest
+            fi
         else
             docker push $DOCKER_IMAGE:$TARGET-$BRANCH
         fi
