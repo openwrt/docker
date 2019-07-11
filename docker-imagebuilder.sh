eabce025b4703d0cc1fa132a1013aae4803beb82
Lucas Ramage <oxr463@gmx.us>
2019-07-11 14:56:52 -0400

    Use Alpine-base for imagebuilder and sdk
    Bug: https://github.com/openwrt/docker/issues/5

--- docker-imagebuilder.sh
+++ docker-imagebuilder.sh
@@ -6,40 +6,43 @@ set -x
 TARGETS="${TARGETS:-x86-64}"
 BRANCHES="${BRANCHES:-master}"
 DOCKER_IMAGE="${DOCKER_IMAGE:-openwrt-imagebuilder}"
+DISTROS="alpine debian"
 
 for TARGET in $TARGETS ; do
-    export IMAGEBUILDER_FILE="openwrt-imagebuilder*x86_64.tar.xz"
-    for BRANCH in $BRANCHES; do
-        if [ "$BRANCH" == "master" ]; then
-            export IMAGEBUILDER_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
-        else
-            export IMAGEBUILDER_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
-        fi
-
-        curl "https://downloads.openwrt.org/$IMAGEBUILDER_PATH/sha256sums" -sS -o sha256sums
-        curl "https://downloads.openwrt.org/$IMAGEBUILDER_PATH/sha256sums.asc" -sS -o sha256sums.asc
-        gpg --with-fingerprint --verify sha256sums.asc sha256sums
-        rsync -av "downloads.openwrt.org::downloads/$IMAGEBUILDER_PATH/$IMAGEBUILDER_FILE" . || contine # skip uploading if no IB is available
-        grep openwrt-imagebuilder sha256sums > sha256sums_imagebuilder
-        sha256sum -c sha256sums_imagebuilder
-
-        mkdir -p ./imagebuilder
-        tar Jxf $IMAGEBUILDER_FILE --strip=1 -C ./imagebuilder
-        rm -rf $IMAGEBUILDER_FILE
-
-        docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f Dockerfile.imagebuilder .
-
-        rm -rf ./imagebuilder
-
-        if [ "$BRANCH" == "master" ]; then
-            docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
-            docker push "$DOCKER_IMAGE:$TARGET"
-            if [ "$TARGET" == "x86-64" ]; then
-                docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:latest"
-                docker push "$DOCKER_IMAGE:latest"
+    for DISTRIBUTION in "${DISTROS}" ; do
+        export IMAGEBUILDER_FILE="openwrt-imagebuilder*x86_64.tar.xz"
+        for BRANCH in $BRANCHES; do
+            if [ "$BRANCH" == "master" ]; then
+                export IMAGEBUILDER_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
+            else
+                export IMAGEBUILDER_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
             fi
-        else
-            docker push "$DOCKER_IMAGE:$TARGET-$BRANCH"
-        fi
+
+            curl "https://downloads.openwrt.org/$IMAGEBUILDER_PATH/sha256sums" -sS -o sha256sums
+            curl "https://downloads.openwrt.org/$IMAGEBUILDER_PATH/sha256sums.asc" -sS -o sha256sums.asc
+            gpg --with-fingerprint --verify sha256sums.asc sha256sums
+            rsync -av "downloads.openwrt.org::downloads/$IMAGEBUILDER_PATH/$IMAGEBUILDER_FILE" . || contine # skip uploading if no IB is available
+            grep openwrt-imagebuilder sha256sums > sha256sums_imagebuilder
+            sha256sum -c sha256sums_imagebuilder
+
+            mkdir -p ./imagebuilder
+            tar Jxf $IMAGEBUILDER_FILE --strip=1 -C ./imagebuilder
+            rm -rf $IMAGEBUILDER_FILE
+
+            docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f imagebuilder/"Dockerfile.${DISTRIBUTION}" .
+
+            rm -rf ./imagebuilder
+
+            if [ "$BRANCH" == "master" ]; then
+                docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
+                docker push "$DOCKER_IMAGE:$TARGET"
+                if [ "$TARGET" == "x86-64" ]; then
+                    docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:latest"
+                    docker push "$DOCKER_IMAGE:latest"
+                fi
+            else
+                docker push "$DOCKER_IMAGE:$TARGET-$BRANCH"
+            fi
+        done
     done
 done
--- docker-sdk.sh
+++ docker-sdk.sh
@@ -6,31 +6,33 @@ set -x
 TARGETS="${TARGETS:-x86-64}"
 BRANCHES="${BRANCHES:-master}"
 DOCKER_IMAGE="${DOCKER_IMAGE:-openwrt-sdk}"
+DISTROS="alpine debian"
 
 for TARGET in $TARGETS ; do
-    SDK_FILE="openwrt-sdk-*.Linux-x86_64.tar.xz"
-    for BRANCH in $BRANCHES; do
-        if [ "$BRANCH" == "master" ]; then
-            SDK_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
-            BRANCH_FEEDS="$BRANCH"
-        else
-            SDK_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
-            BRANCH_FEEDS="openwrt-$BRANCH"
-        fi
+    for DISTRIBUTION in "${DISTROS}" ; do
+        SDK_FILE="openwrt-sdk-*.Linux-x86_64.tar.xz"
+        for BRANCH in $BRANCHES; do
+            if [ "$BRANCH" == "master" ]; then
+                SDK_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
+                BRANCH_FEEDS="$BRANCH"
+            else
+                SDK_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
+                BRANCH_FEEDS="openwrt-$BRANCH"
+            fi
 
-        curl "https://downloads.openwrt.org/$SDK_PATH/sha256sums" -sS -o sha256sums
-        curl "https://downloads.openwrt.org/$SDK_PATH/sha256sums.asc" -sS -o sha256sums.asc
-        gpg --with-fingerprint --verify sha256sums.asc sha256sums
-        rsync -av "downloads.openwrt.org::downloads/$SDK_PATH/$SDK_FILE" . || continue # skip uploading if no SDK is available
-        grep openwrt-sdk sha256sums > sha256sums_sdk
-        sha256sum -c sha256sums_sdk
+            curl "https://downloads.openwrt.org/$SDK_PATH/sha256sums" -sS -o sha256sums
+            curl "https://downloads.openwrt.org/$SDK_PATH/sha256sums.asc" -sS -o sha256sums.asc
+            gpg --with-fingerprint --verify sha256sums.asc sha256sums
+            rsync -av "downloads.openwrt.org::downloads/$SDK_PATH/$SDK_FILE" . || continue # skip uploading if no SDK is available
+            grep openwrt-sdk sha256sums > sha256sums_sdk
+            sha256sum -c sha256sums_sdk
 
-        mkdir -p ./sdk
-        tar Jxf $SDK_FILE --strip=1 -C ./sdk
-        rm -rf $SDK_FILE
+            mkdir -p ./sdk
+            tar Jxf $SDK_FILE --strip=1 -C ./sdk
+            rm -rf $SDK_FILE
 
-        # use GitHub instead of git.openwrt.org
-        cat > ./sdk/feeds.conf <<EOF
+            # use GitHub instead of git.openwrt.org
+            cat > ./sdk/feeds.conf <<EOF
 src-git base https://github.com/openwrt/openwrt.git;$BRANCH_FEEDS
 src-git packages https://github.com/openwrt/packages.git;$BRANCH_FEEDS
 src-git luci https://github.com/openwrt/luci.git;$BRANCH_FEEDS
@@ -38,19 +40,20 @@ src-git routing https://github.com/openwrt-routing/packages.git;$BRANCH_FEEDS
 src-git telephony https://github.com/openwrt/telephony.git;$BRANCH_FEEDS
 EOF
 
-        docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f Dockerfile.sdk .
+            docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f sdk/"Dockerfile.${DISTRIBUTION}" .
 
-        rm -rf ./sdk
+            rm -rf ./sdk
 
-        if [ "$BRANCH" == "master" ]; then
-            docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
-            docker push "$DOCKER_IMAGE:$TARGET"
-            if [ "$TARGET" == "x86-64" ]; then
-                docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:latest"
-                docker push "$DOCKER_IMAGE:latest"
+            if [ "$BRANCH" == "master" ]; then
+                docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
+                docker push "$DOCKER_IMAGE:$TARGET"
+                if [ "$TARGET" == "x86-64" ]; then
+                    docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:latest"
+                    docker push "$DOCKER_IMAGE:latest"
+                fi
+            else
+                docker push "$DOCKER_IMAGE:$TARGET-$BRANCH"
             fi
-        else
-            docker push "$DOCKER_IMAGE:$TARGET-$BRANCH"
-        fi
+        done
     done
 done
