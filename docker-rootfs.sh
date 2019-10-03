7a3aaf3fa0798868adfc5ce75c174fd931d81544
Paul Spooren <mail@aparcar.org>
2019-10-02 16:59:12 -1000

    introduce gen-targets.sh script
    This improves readability as it auto generates the target list.

--- docker-rootfs.sh
+++ docker-rootfs.sh
@@ -2,23 +2,20 @@
 
 set -ex
 
-TARGETS="${TARGETS:-x86-64}"
+export TARGET="${TARGET:-x86-64}"
 export BRANCH="${BRANCH:-master}"
 export DOCKER_IMAGE="${DOCKER_IMAGE:-openwrt-rootfs}"
 export DOWNLOAD_FILE="openwrt-*-rootfs.tar.gz"
 export DOCKERFILE="Dockerfile.rootfs"
 
-for TARGET in $TARGETS ; do
-    export TARGET
-    if [ "$BRANCH" == "master" ]; then
-        export DOWNLOAD_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
-    else
-        export DOWNLOAD_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
-    fi
+if [ "$BRANCH" == "master" ]; then
+	export DOWNLOAD_PATH="snapshots/targets/$(echo $TARGET | tr '-' '/')"
+else
+	export DOWNLOAD_PATH="releases/$BRANCH/targets/$(echo $TARGET | tr '-' '/')"
+fi
 
-    ./docker-download.sh || continue
+./docker-download.sh || continue
 
-    cp -r ./rootfs/* ./build
+cp -r ./rootfs/* ./build
 
-    ./docker-build.sh || exit 1
-done
+./docker-build.sh || exit 1
