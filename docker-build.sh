2176ce831102db31b1f70090baa9461ec8f524fc
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2020-05-20 12:10:33 +0200

    docker-build: Fix compatibility with older Docker versions

--- docker-build.sh
+++ docker-build.sh
@@ -3,7 +3,10 @@
 set -ex
 
 DOCKERFILE="${DOCKERFILE:-Dockerfile}"
-docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f "$DOCKERFILE" ./build
+# Copy Dockerfile inside build context to support older Docker versions
+# See https://github.com/docker/cli/pull/886
+cp "$DOCKERFILE" ./build/
+docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f "./build/$DOCKERFILE" ./build
 
 if [ "$BRANCH" == "master" ]; then
     docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
