3efde3b022c23ce6adb5a9b7bbbaa981609c7ddc
Paul Spooren <mail@aparcar.org>
2020-07-13 18:57:20 -1000

    dynamic downstream job creation based on targets
    Instead of having a hard coded list of available targets, this approach
    makes use os the dumpinfo.pl script that automatically finds available
    targets existing in the OpenWrt buildroot. The advantage is to
    automatically adapt to available targets in different branches.
    
    Additionally the tagging of SDK containers is improved. While it is
    still possible to use SDK based on target tag, it is now also possible
    to chose the target based on architecutre. This way less SDK container
    are build as one architecutre covers multiple targets.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- docker-sdk.sh
+++ docker-sdk.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-TARGET=$(echo "$CI_JOB_NAME" | cut -d _ -f 2-)
+ARCH=$(echo "$CI_JOB_NAME" | cut -d _ -f 2-)
+# shellcheck disable=SC2153
+TARGET=$(echo "$TARGETS" | cut -d ' ' -f 1)
 export TARGET="${TARGET:-x86-64}"
 export BRANCH="${BRANCH:-master}"
 export DOCKER_IMAGE="${DOCKER_IMAGE:-openwrt-sdk}"
@@ -16,4 +18,26 @@ fi
 export DOWNLOAD_PATH
 
 ./docker-download.sh || exit 1
-./docker-build.sh || exit 1
+
+DOCKERFILE="${DOCKERFILE:-Dockerfile}"
+# Copy Dockerfile inside build context to support older Docker versions
+# See https://github.com/docker/cli/pull/886
+cp "$DOCKERFILE" ./build/
+docker build -t "$DOCKER_IMAGE:$ARCH-$BRANCH" -f "./build/$DOCKERFILE" ./build
+
+if [ "$BRANCH" == "master" ]; then
+    docker tag "$DOCKER_IMAGE:$ARCH-$BRANCH" "$DOCKER_IMAGE:$ARCH"
+    if [ "$ARCH" == "x86_64" ]; then
+        docker tag "$DOCKER_IMAGE:$ARCH-$BRANCH" "$DOCKER_IMAGE:latest"
+    fi
+fi
+
+if [ "$ARCH" == "x86_64" ]; then
+    docker tag "$DOCKER_IMAGE:$ARCH-$BRANCH" "$DOCKER_IMAGE:$BRANCH"
+fi
+
+for TARGET_TAG in $TARGETS; do
+    docker tag "$DOCKER_IMAGE:$ARCH-$BRANCH" "$DOCKER_IMAGE:$TARGET_TAG-$BRANCH"
+done
+
+rm -rf ./build
