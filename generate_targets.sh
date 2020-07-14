ab8551250874db17ef551ff5fa5ed8ce869979fd
Paul Spooren <mail@aparcar.org>
2020-07-13 18:57:20 -1000

    dynamic downstream job creation based on targets
    Instead of having a hard coded list of available targets, this approach
    makes use os the dumpinfo.pl script that automatically finds available
    targets existing in the OpenWrt buildroot. The advantage is to
    automatically adapt to available targets in different branches.
    
    Additionally the tagging of SDK containers is improved. While it is
    still possible to use SDK based on target tag, it is now also possible
    to chose the target based on architecutre. This way less SDK container
    are build as one architecutre covers multiple targets.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

---
+++ generate_targets.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+gen_targets_sdk() {
+	perl ./scripts/dumpinfo.pl architectures | while read -r LINE; do
+		ARCH=$(echo "$LINE" | cut -d ' ' -f 1)
+		TARGETS=$(echo "$LINE" | cut -d ' ' -f 2-)
+		echo "
+deploy-sdk_$ARCH:
+  extends: .deploy-sdk
+  variables:
+    TARGETS: $TARGETS"
+	done
+}
+
+gen_targets_imagebuilder() {
+	perl ./scripts/dumpinfo.pl targets | while read -r LINE; do
+		TARGET=$(echo "$LINE" | cut -d ' ' -f 1 | tr '/' '-')
+		echo "
+deploy-imagebuilder_$TARGET:
+  extends: .deploy-imagebuilder"
+	done
+}
+
+echo "include:
+  - local: .gitlab/ci/deploy.yml
+" > ../targets.yml
+gen_targets_sdk >> ../targets.yml
+gen_targets_imagebuilder >> ../targets.yml
+cat ../targets.yml
