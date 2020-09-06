048ab0a7f6c8be537fc0d309bdd627747c40ee9f
Paul Spooren <mail@aparcar.org>
2020-09-06 12:20:54 -1000

    support ARCH tagging
    For the rootfs containers it can be of interest to run a root container
    based on used architecture. Say a CI builds a specific architecture and
    a container should run the specific packages. Instead of maintaining the
    mapping upstream this should happen via container tags.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- docker-build.sh
+++ docker-build.sh
@@ -8,7 +8,14 @@ DOCKERFILE="${DOCKERFILE:-Dockerfile}"
 cp "$DOCKERFILE" ./build/
 docker build -t "$DOCKER_IMAGE:$TARGET-$BRANCH" -f "./build/$DOCKERFILE" ./build
 
+if [ -n "$ARCH" ]; then
+    docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$ARCH-$BRANCH"
+fi
+
 if [ "$BRANCH" == "master" ]; then
+    if [ -n "$ARCH" ]; then
+        docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$ARCH"
+    fi
     docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:$TARGET"
     if [ "$TARGET" == "x86-64" ]; then
         docker tag "$DOCKER_IMAGE:$TARGET-$BRANCH" "$DOCKER_IMAGE:latest"
